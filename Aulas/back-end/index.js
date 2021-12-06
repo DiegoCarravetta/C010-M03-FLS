0aca1e353bf2f6865485e45366a73d44b3c3ed40
Diego Carravetta <diegocarravetta@icloud.com>
2021-12-06 20:08:12 -0300

    Aula 6

--- Aulas/back-end/index.js
+++ Aulas/back-end/index.js
@@ -1,75 +1,37 @@
-// Preciso importar o express para que seja possível ultilizar as suas funções na aplicação
-const express = require("express");
+// SERVER (API)
+// importar o express para que seja possivel ultilizar as suas funcoes na nossa aplicacao
+const express = require('express');
 
-// Depois de importar o express, preciso ativá-lo
+//importo o lib cors 
+const cors = require('cors')
+
+// inicilizar o express no nosso arquivo javascript para que ele possa assumir as funcoes do servidor
 const app = express();
 
-// Preciso importar o JSON (Javascript Object Notation) para trabalhar com o express
+//CORS - permite a troca de recursos entre origens diferentes
+// usamos o middleware da lib cors
+app.use(cors());
+
+// JSON - Javascript Object Notation
+// falo pro express trabalhar com middleware de json para trabalharmos com o formato JSON.
 app.use(express.json());
 
-// Iniciando meus dados fixos manualmente (CRUD memória)
-const blueVagas = [
-    {
-        id: 1,
-        empresa: "Blue",
-        salario: 3000,
-        oportunidade: "Front-End Jr",
-        tipo: "estágio"
-    },
+//importo o arquivo de rotas
 
-    {
-        id: 2,
-        empresa: "Google",
-        salario: 10000,
-        oportunidade: "Front-End Jr",
-        tipo: "CLT"
-    },
+const vagasRouter = require('./routes/vagas.route');
 
-    {
-        id: 3,
-        empresa: "Facebook",
-        salario: 20000,
-        oportunidade: "Full Stack Sr",
-        tipo: "PJ"
-    },
-    
-    {
-        id: 4,
-        empresa: "Amazon",
-        salario: 15000,
-        oportunidade: "Full Stack Pl",
-        tipo: "CLT"
-    }
-]
+// falo pro backend usar as minha rotas para o endpoint /vagas
+app.use('/vagas', vagasRouter);
 
-//CORS - Protege a sua aplicação, permitindo a troca de recursos entre origens diferentes
-app.all('/*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    next();
-})
 
-// Preciso criar a rota principal com req e res, pois toda rota envia e recebe informação
-//REQ (REQUEST/REQUISICAO) - VEM DO CLIENTE
-//RES (RESPONSE/RESPOSTA) - VOLTA PARA O CLIENTE
-app.get('/', (req, res) => {
-    res.send('Olá Blumers!');
-});
 
-// Crio a rota onde a regra de negócio retornar a lista de vagas
-app.get('/vagas', (req, res) => {
-    res.send(blueVagas);
-})
 
-// Crio uma rota que retorna para o cliente uma única vaga de acordo com o seu id, procurando na lista uma vaga que contenha o id igual ao que eu recebi via parametro
-app.get('/vagas/:id', (req, res) => {
-    const idParam = req.params.id; //acessar o id via a requisicao
-    const vagaEcontrada = blueVagas.find(vaga => vaga.id == idParam); // busca o item na lista de acordo com o seu id
-    res.send(vagaEcontrada); // envia para o front-end a vaga encontrada
-})
 
-// Preciso definir a porta que o meu backend irá executar e inicializar o servidor com ela
+
+// Definir a porta que o meu backend irá executar
 const port = 3000;
+
+// inicializa o servidor na seguite porta
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`App Rodando na porta ${port}`);
 })
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Estou trabalhando com Node, preciso instalar o package.json: npm init -y
 Precisa instalar a biblioteca Nodemon: npm i nodemon -D
 Precisa instalar a biblioteca Express: npm i express
 Em package.json, no campo script, escrever a linha: "dev": "nodemon index",
+Precisa instalar a biblioteca Cors: npm i cors
